4528195bad434e1b16180f15ba9471946e46710f
NotHiaki <celsohenrique367@gmail.com>
2024-01-10 20:05:52 -0300

    feat: random faces title

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  face: string = '';
 
+  ngOnInit(): void {
+    const arr: string[] = [':)', ';)', ':3', ':D', ':P', '<3'];
+    
+    this.face = arr[Math.floor(Math.random() * arr.length)];
+  }
 }
