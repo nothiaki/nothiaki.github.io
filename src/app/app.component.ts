bda1e1a541e59a720b78773e4aabd55c26c4b22c
NotHiaki <celsohenrique367@gmail.com>
2024-01-10 22:17:46 -0300

    feat: footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./components/home/home.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, FooterComponent]
 })
 export class AppComponent {
 
