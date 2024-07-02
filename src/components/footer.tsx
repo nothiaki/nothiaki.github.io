1fb6b0308e66beb6d0990eca8f98019778770aa9
nothiaki <celsohenrique367@gmail.com>
2024-07-01 22:56:20 -0300

    feat: footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <>
+      <div className="flex flex-col">
+        <p>&copy; Celso Henrique 2024 <span className="text-primary">::</span> All rights reserved.</p>
+      </div>
+    </>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import { Articles } from "../components/articles"
+import { Footer } from "../components/footer"
 import { Projects } from "../components/projects"
 import { Social } from "../components/social"
 import { Title } from "../components/title"
@@ -7,11 +8,12 @@ export function Home() {
   return (
     <>
       <div className="min-h-screen bg-background text-text">
-        <main className="px-64 py-24 flex flex-col gap-9">
+        <main className="px-12 pt-24 pb-4 flex flex-col gap-9 md:px-64">
           <Title />
           <Articles />
           <Projects />
           <Social />
+          <Footer />
         </main>
       </div>
     </>
