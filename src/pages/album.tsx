2982f344d661f9b336a95eb9b0e40a0f636d5f4c
nothiaki <celsohenrique367@gmail.com>
2024-07-28 20:42:55 -0300

    fix: images in gh pages

--- src/pages/album.tsx
+++ src/pages/album.tsx
@@ -25,14 +25,14 @@ export function Album() {
               className="columns-2 gap-5 lg:gap-8 lg:columns-3
                 xl:columns-4 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8"
             >
-              <img src="/src/assets/album/01.jpg" loading="lazy" />
-              <img src="/src/assets/album/02.jpg" loading="lazy" />
-              <img src="/src/assets/album/03.jpg" loading="lazy" />
-              <img src="/src/assets/album/04.jpg" loading="lazy" />
-              <img src="/src/assets/album/05.jpg" loading="lazy" />
-              <img src="/src/assets/album/06.jpg" loading="lazy" />
-              <img src="/src/assets/album/07.jpg" loading="lazy" />
-              <img src="/src/assets/album/08.jpg" loading="lazy" />
+              <img src="src/assets/album/01.jpg" loading="lazy" />
+              <img src="src/assets/album/02.jpg" loading="lazy" />
+              <img src="src/assets/album/03.jpg" loading="lazy" />
+              <img src="src/assets/album/04.jpg" loading="lazy" />
+              <img src="src/assets/album/05.jpg" loading="lazy" />
+              <img src="src/assets/album/06.jpg" loading="lazy" />
+              <img src="src/assets/album/07.jpg" loading="lazy" />
+              <img src="src/assets/album/08.jpg" loading="lazy" />
             </div>
           </div>
           <Footer />
