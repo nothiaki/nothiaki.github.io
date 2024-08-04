ead0cb82bb5b43d0698e0cec9bf27a8cfaa89daf
nothiaki <celsohenrique367@gmail.com>
2024-08-04 18:43:39 -0300

    refactor: remove album page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route, Navigate, HashRouter } from "react-router-dom"
 import { Home } from "./pages/home"
-import { Album } from "./pages/album"
 
 export function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={ <Home />} />
-        <Route path="/album" element={ <Album />} />
         <Route path="*" element={ <Navigate to={"/"} />} />
       </Routes>
     </HashRouter>
